d7e354c280c738fc2d483204a25f22683356ff0d
Pedro Bautista <bautista17@usf.edu>
2023-04-06 00:34:49 -0400

    added user authentication state across entire application. Might need to modify since it is in typescript.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "../styles/globals.css"
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
 
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+  );
+};
+
+export default App;
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
