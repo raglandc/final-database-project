9409d42cd152bb45c46f02668ebc53d4a971053a
Pedro Bautista <bautista17@usf.edu>
2023-04-06 00:53:54 -0400

    Another change to schema.prisma which fixes nextauth. Changed code in [...nextauth].js It is probably in javascript now instead of typescript.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,16 +4,23 @@ import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from "next-auth/providers/google"
+//import { PrismaClient } from "@prisma/client"
 import prisma from '../../../lib/prisma';
 
+//const prisma = new PrismaClient()
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
-
 const options = {
+
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
     adapter: PrismaAdapter(prisma),
