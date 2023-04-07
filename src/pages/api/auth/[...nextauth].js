65325950bd4381f4dad2b913469446ee17e955f3
Chris Ragland <Chrisragland97@gmail.com>
2023-04-07 12:45:48 -0400

    add github login

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,17 +5,16 @@ import { PrismaClient } from "@prisma/client"
 
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from "next-auth/providers/google"
-// import prisma from '../../../../lib/prisma';
 
 const prisma = new PrismaClient()
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
-        // GitHubProvider({
-        //         clientId: process.env.GITHUB_ID,
-        //         clientSecret: process.env.GITHUB_SECRET,
-        // }),
+        GitHubProvider({
+                clientId: process.env.GITHUB_ID,
+                clientSecret: process.env.GITHUB_SECRET,
+        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
