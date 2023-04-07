d6d89fbedd645dbe0868f9ce9dc0e00a4fd85826
Chris Ragland <Chrisragland97@gmail.com>
2023-04-07 12:45:01 -0400

    user auth sign in and sign out is working properly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,13 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+
+import { signOut } from "next-auth/react"
 
 export default function NavBar()
 {
+  //useRouter to update menu depending upon user location
+  const router = useRouter();
+
   return (
     <div className="w-screen py-5 bg-teal-900 text-white">
       <div className="flex m-auto items-center justify-between w-9/12">
@@ -11,12 +17,13 @@ export default function NavBar()
           </div>
         </Link>
         <div>
-          <Link href="/watch-list" className="bg-teal-600 px-3 py-1 rounded-sm hover:cursor-pointer hover:bg-teal-500">
-            My Watch List
+          <Link href={router.asPath == "/watch-list" ? "/" : "/watch-list"} className="bg-teal-600 px-3 py-1 rounded-sm hover:cursor-pointer hover:bg-teal-500">
+            {router.asPath == "/watch-list" ? "Dashboard" : "My Watch List" }
           </Link>
-          <Link href="/login" className="px-3 py-1 hover:underline hover:cursor-pointer">
+          <button onClick={() => signOut({callbackUrl: "/login"})}
+                  className="px-3 py-1 hover:underline hover:cursor-pointer">
             Sign Out
-          </Link>
+          </button>
         </div>
       </div>
     </div>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,4 @@
+import { signIn } from 'next-auth/react';
 import prisma from '../../lib/prisma';
 
 export default function Login()
@@ -7,8 +8,14 @@ export default function Login()
       <div className="shadow-lg m-auto flex flex-col min-h-2/4 p-10 bg-white rounded-sm w-1/4 items-center">
         <h1 className="text-teal-600 text-6xl w-full">Bulls Movies</h1>
         <p className="my-5 text-stone-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quidem itaque ab harum aliquam ex assumenda natus aspernatur debitis laborum. Quos dicta et at molestiae amet exercitationem dignissimos deleniti eligendi.</p>
-        <button className="text-white px-3 py-2 bg-teal-600 rounded-md shadow-sm hover:bg-teal-500 hover:scale-105 active:scale-95">Login / Sign up</button>
-      </div>
+
+        <button 
+          onClick={() => signIn(undefined, {callbackUrl: "/"})}
+          className="text-white px-3 py-2 bg-teal-600 rounded-md shadow-sm hover:bg-teal-500 hover:scale-105 active:scale-95"
+          >
+          Login / Sign up
+        </button>      
+        </div>
       <div className="h-screen bg-login-hero-image w-2/4 bg-cover opacity-80" />
     </div>
   )
