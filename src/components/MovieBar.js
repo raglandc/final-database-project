ef273b6a75fb3a35ae6bb11734295cda97dbdd6c
Chris Ragland <Chrisragland97@gmail.com>
2023-04-11 16:05:09 -0400

    movie info cards basic creation

---
+++ src/components/MovieBar.js
@@ -0,0 +1,17 @@
+export default function MovieBar({title, rating, overview})
+{
+  return (
+    <div className="border rounded-md shadow-sm overflow-hidden m-auto w-10/12 my-2">
+      <div className="flex p-4 bg-teal-800 justify-between">
+        <h1 className="text-white text-2xl">{title}</h1>
+        <div className="flex items-center">
+          <p className="mx-2 text-white hover:text-green-500 hover:cursor-pointer">
+            +watch list
+          </p>
+          <p className="text-white text-2xl">{rating}</p>
+        </div>
+      </div>
+        <p className="p-4">{overview}</p>
+    </div>
+  )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,13 @@ export default function NavBar()
   return (
     <div className="w-screen h-16 bg-teal-900 text-white">
       <div className="flex h-full m-auto items-center justify-between w-9/12">
-        <Link href="/" className="hover:text-green-500">
+        <Link href="/dashboard" className="hover:text-green-500">
             Bulls🍿Movies
         </Link>
         <div className="flex items-center h-full">
           <NavLink link="/watch-list" title="Watch List" />
-          <NavLink link="/" title="Dashboard" />
-          <button className="px-3 h-full flex items-center rounded-sm hover:border-b-8 hover:border-b-green-500 hover:cursor-pointer" onClick={() => signOut({callbackUrl: "/login"})}>Sign out</button>
+          <NavLink link="/dashboard" title="Dashboard" />
+          <button className="px-3 h-full flex items-center rounded-sm hover:border-b-8 hover:border-b-green-500 hover:cursor-pointer" onClick={() => signOut({callbackUrl: "/"})}>Sign out</button>
         </div>
       </div>
     </div>
