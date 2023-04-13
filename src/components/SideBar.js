89c74a04d495c185073fdb20478df02d40fbf66c
Chris Ragland <Chrisragland97@gmail.com>
2023-04-13 07:55:59 -0400

    remove genre filter

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,14 +6,6 @@ export default function SideBar()
         <h2 className="text-4xl my-3">Search</h2>
         <label for="title" className="mt-1 text-white">Title</label>
         <input type="text" id="title" placeholder="Title" className="bg-transparent text-white border-b-4 my-1 p-3 rounded-sm hover:border-green-600 hover:cursor-pointer"/>
-        <label className="mt-1" htmlFor="genre">Genre</label>
-        <select name="genre" id="genre" type="select" className="bg-transparent border-b-4 p-3 rounded-sm my-2 hover:border-green-600 hover:cursor-pointer active:border-green-600">
-          <option selected>--Select--</option>
-          <option>action</option>
-          <option>horror</option>
-          <option>sci-fi</option>
-          <option>comedy</option>
-        </select>
         <label htmlFor="year" className="mt-1 ">Year</label>
         <input type="text" id="year" placeholder="Year" className="my-1 bg-transparent hover:cursor-pointer border-b-4 p-3 rounded-sm hover:border-green-600"/>
         <label className="mt-1" htmlFor="rating">Rating</label>
