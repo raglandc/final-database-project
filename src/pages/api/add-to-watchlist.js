8722770426b0727bd611824de97e7c327116908e
Chris Ragland <Chrisragland97@gmail.com>
2023-04-19 07:53:50 -0400

    add try catch blocks to api routes for watchlist calls

--- src/pages/api/add-to-watchlist.js
+++ src/pages/api/add-to-watchlist.js
@@ -2,29 +2,37 @@ import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res)
 {
-  const { email, movieId } = await req.body;
-
-  //get user info
-  const user = await prisma.user.findUnique({
-    where : {
-      email: email
-    }
-  })
-
-  const userId = user.id;
-
-  if (movieId)
+  try
   {
-    await prisma.movieWatchlist.create({
-      data: {
-        userId,
-        movieId,
+    const { email, movieId } = await req.body;
+
+    //get user info
+    const user = await prisma.user.findUnique({
+      where : {
+        email: email
       }
     })
-  }
-  else {
-    //respond with error
-  }
 
-  res.status(200);
+    const userId = user.id;
+
+    if (movieId)
+    {
+      await prisma.movieWatchlist.create({
+        data: {
+          userId,
+          movieId,
+        }
+      })
+    }
+    else {
+      //respond with error
+    }
+
+    res.status(200).json({message: "Successfully added to watchlist"});
+  }
+  catch (error)
+  {
+    console.error(error);
+    res.status(500).json({error: "Failed to add to watchlist"})
+  }
 }
--- src/pages/api/get-watchlist.js
+++ src/pages/api/get-watchlist.js
@@ -2,25 +2,33 @@ import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res)
 {
-  const { email, movieId } = await req.body;
+  try
+  {
+    const { email, movieId } = await req.body;
 
-  //get user info
-  const user = await prisma.user.findUnique({
-    where : {
-      email: email
-    }
-  })
+    //get user info
+    const user = await prisma.user.findUnique({
+      where : {
+        email: email
+      }
+    })
 
-  const userId = user.id;
+    const userId = user.id;
 
-  const movie = await prisma.movieWatchlist.findFirst({
-    where: {
-      userId, 
-      movieId 
-    }
-  })
+    const movie = await prisma.movieWatchlist.findFirst({
+      where: {
+        userId, 
+        movieId 
+      }
+    })
 
-  const isMovieInWatchList = !!movie;
+    const isMovieInWatchList = !!movie;
 
-  res.status(200).json({ isMovieInWatchList });
+    res.status(200).json({ isMovieInWatchList });
+  }
+  catch (error)
+  {
+    console.log(error);
+    res.status(500).json({error: "Failed to get from watchlist"})
+  }
 }
